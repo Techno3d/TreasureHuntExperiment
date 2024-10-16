613ce14fb664a84a53efc048ff4c5b3d17a42486
shadman syed <syeds1>
2024-10-16 13:25:00 -0400

    Collisions are making me cry guys

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float speed = 5f;
     [SerializeField]
-    float detectionDistance = 2f;
+    float detectionDistance = 10f;
     int pointIndex = 0;
     Vector3 vel = Vector3.zero;
     // Start is called before the first frame update
@@ -21,26 +21,39 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = (waypoints[pointIndex].transform.position - transform.position).normalized;
-        transform.rotation = Quaternion.Euler(0f, Mathf.Atan2(dir.z,dir.x), 0f);
-        vel.x = Mathf.MoveTowards(vel.x, dir.x*speed, 20f*Time.deltaTime);
-        vel.z = Mathf.MoveTowards(vel.z, dir.z*speed, 20f*Time.deltaTime);
-        RaycastHit hit;
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        if (Physics.Raycast(transform.position, forward, out hit, detectionDistance)){
-            if (hit.collider.CompareTag("Wall")){
-                SelectNewWaypoint();
-//you can change this to make your agent do something else!
-            }
+        if(Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position)<2f) {
+            chase();
+        } else {
+            patrol();
         }
-
-        SelectNewWaypoint();
-        transform.position += vel*Time.deltaTime;
     }
 
-    void SelectNewWaypoint() {
+    void patrol() {
+        Vector3 dir = (waypoints[pointIndex].transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.Euler(0f, Mathf.Atan2(dir.x,dir.z), 0f);
+        vel.x = Mathf.MoveTowards(vel.x, dir.x*speed, 20f*Time.deltaTime);
+        vel.z = Mathf.MoveTowards(vel.z, dir.z*speed, 20f*Time.deltaTime);
+
         if(Vector2.Distance(waypoints[pointIndex].transform.position, transform.position) < 0.5f) {
             pointIndex = (pointIndex+1)%waypoints.Count;
         }
+        transform.position += vel*Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.collider.CompareTag("Wall")) {
+            Debug.Log("hi");
+            pointIndex = (pointIndex+1)%waypoints.Count;
+            Debug.Log(pointIndex);
+        }
+    }
+
+    void chase() {
+        Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = new Vector3(
+            Mathf.MoveTowards(transform.position.x, playerPos.x, speed * Time.deltaTime),
+            transform.position.y,
+            Mathf.MoveTowards(transform.position.z, playerPos.z, speed * Time.deltaTime)
+        );
     }
 }
